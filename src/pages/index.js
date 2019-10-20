ff4d29b4ddace2df1dedcdf8f2b98237a5a38b65
Paulo Elias <paulo@pauloelias.com>
2019-10-20 08:43:16 -0700

    Remove comment

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import styled from "@emotion/styled"
 import tw from "tailwind.macro"
 import React from "react"
 
-// Adding the TW `container` class errors out, even when including TW via PostCSS
 const Wrapper = styled.div`
   ${tw`p-64 bg-gray-200`}
 `
