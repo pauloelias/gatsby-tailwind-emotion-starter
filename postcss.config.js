02f04d79f801837f3bf212fdd6ce7f6183a0ae53
Paulo Elias <paulo@ideo.com>
2019-10-20 10:56:35 -0700

    Remove purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,3 @@
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./src/**/*.js", "./src/**/*.jsx"],
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-})
-
 module.exports = () => ({
-  plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
-  ],
+  plugins: [require("tailwindcss"), require("autoprefixer")],
 })
