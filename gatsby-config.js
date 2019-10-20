c94e998cf17dbc82cba4706b912ada802636645d
Paulo Elias <paulo@ideo.com>
2019-10-20 09:20:15 -0700

    Add description and project meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
 module.exports = {
+  siteMetadata: {
+    title: `Gatsby Tailwind CSS + Emotion Starter`,
+    description: `A bare-bones Tailwind CSS + Emotion starter to kickoff your project. `,
+    author: `@pauloelias`,
+  },
   plugins: [`gatsby-plugin-postcss`, `gatsby-plugin-emotion`],
 }
