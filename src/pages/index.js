4c5fe91f134d29630921c60685926c2bb816249d
Paulo Elias <paulo@ideo.com>
2019-10-20 15:03:59 -0700

    Add and style logos

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,21 @@
 import tw from "tailwind.macro"
 import React from "react"
 
+const logos = [
+  {
+    name: "gatsby",
+    logo: require("../assets/gatsby.svg"),
+  },
+  {
+    name: "tailwind",
+    logo: require("../assets/tailwind.svg"),
+  },
+  {
+    name: "emotion",
+    logo: require("../assets/emotion.png"),
+  },
+]
+
 const Wrapper = tw.div`
   flex items-center justify-center h-screen
 `
@@ -17,11 +32,25 @@ const Text = tw.p`
   text-gray-700
 `
 
+const Logos = tw.div`
+  flex items-center justify-around mt-4 px-12
+`
+
+const Icon = tw.img`
+  h-12
+`
+
 export default () => (
   <Wrapper>
     <Main>
       <Heading>Hello, world!</Heading>
       <Text>Welcome to the Gatsby Tailwind CSS + Emotion Starter.</Text>
+      <Logos>
+        {logos &&
+          logos.map(({ name, logo }, index) => (
+            <Icon src={logo} alt={`${name} Logo`} key={index} />
+          ))}
+      </Logos>
     </Main>
   </Wrapper>
 )
