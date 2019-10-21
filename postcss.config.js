7b1bd285941e325ac05103418ab695e0b7fa0099
Paulo Elias <paulo@ideo.com>
2019-10-20 17:54:20 -0700

    Remove autoprefixer and bump version

--- postcss.config.js
+++ postcss.config.js
@@ -3,10 +3,9 @@ const postcssPresetEnv = require(`postcss-preset-env`)
 module.exports = () => ({
   plugins: [
     require("tailwindcss"),
-    require("autoprefixer"),
     postcssPresetEnv({
       browsers: "last 2 versions",
-      stage: 3,
+      stage: 2,
     }),
   ],
 })
