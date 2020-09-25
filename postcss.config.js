c8a19df92b6ef3f38521ebd8b1017c80d6be707d
Paulo Elias <paulo@pauloelias.com>
2020-09-25 11:05:52 -0700

    Improve PostCSS config

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,15 @@
 module.exports = {
   plugins: [
-    require("postcss-import"),
     require("tailwindcss")("./tailwind.config.js"),
+    require("postcss-flexbugs-fixes"),
+    require("postcss-import"),
     require("postcss-preset-env")({
-      autoprefixer: { grid: true },
+      autoprefixer: {
+        flexbox: "no-2009",
+      },
+      stage: 3,
       features: {
-        "nesting-rules": true,
+        "custom-properties": false,
       },
       browsers: ["> 1%", "last 2 versions", "Firefox ESR"],
     }),
